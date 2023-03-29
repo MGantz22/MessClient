d64c357386a326154ef3366672318b7b7ae4da85
Mitchell Gantz <mgantz22@icloud.com>
2023-03-29 13:54:35 -0700

    Create Api helper

--- MessClient/Models/ApiHelper.cs
+++ MessClient/Models/ApiHelper.cs
@@ -20,5 +20,31 @@ namespace MessClient.Models
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
+
+    public static async void Post(string newPost)
+    {
+      RestClient client = new RestClient("http://localhost:7201/");
+      RestRequest request = new RestRequest($"api/posts", Method.Post);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newPost);
+      await client.PostAsync(request);
+    }
+
+    public static async void Put(int id, string newPost)
+    {
+      RestClient client = new RestClient("http://localhost:7201/");
+      RestRequest request = new RestRequest($"api/post/{id}", Method.Put);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newPost);
+      await client.PutAsync(request);
+    }
+
+    public static async void Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:7201/");
+      RestRequest request = new RestRequest($"api/Post/{id}", Method.Delete);
+      request.AddHeader("Content-Type", "application/json");
+      await client.DeleteAsync(request);
+    }
   }
 }
