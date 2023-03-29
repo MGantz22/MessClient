184c1c16e916e47496be089bfdc69d3db8443562
Mitchell Gantz <mgantz22@icloud.com>
2023-03-29 13:54:02 -0700

    Add create, Edit and delete functionality
    Co-authored-by: John Lenz <john.lenz27@gmail.com>
    Co-authored-by: erintimli01 <erintimli01@users.noreply.github.com>
    Co-authored-by: Carl Sustrich <CarlSustrich@users.noreply.github.com>

--- MessClient/Controllers/PostsController.cs
+++ MessClient/Controllers/PostsController.cs
@@ -11,9 +11,48 @@ public class PostsController : Controller
     return View(posts);
   }
 
-    public IActionResult Details(int id)
+  public IActionResult Details(int id)
   {
     Post post = Post.GetDetails(id);
     return View(post);
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Post post)
+  {
+    Post.Posted(post);
+    return RedirectToAction("Index");
+
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Post post = Post.GetDetails(id);
+    return View(post);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Post post)
+  {
+    Post.Put(post);
+    return RedirectToAction("Details", new { id = post.PostId });
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Post post = Post.GetDetails(id);
+    return View(post);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Post.Delete(id);
+    return RedirectToAction("Index");
   }
 }
